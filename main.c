d7396d80d273c6ea148351573551bb6afdf262f7
BekxFR <104771705+BekxFR@users.noreply.github.com>
2022-08-09 19:11:05 +0200

    Starter

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+//#define NB 3
+
+int ft_push_swap(int i)
+{
+    if (!i)
+        return (0);
+    return (i);
+}
+
+int main(int argc, char **argv)
+{
+    int i[argc];
+    int j;
+
+    j = -1;
+    while (++j < argc - 1)
+        i[j] = atoi(argv[j + 1]);
+    j = 0;
+    if (argc <= 4)
+
+    while (i[j] && j < argc - 1)
+    {
+        if (j == argc - 2)
+            printf("PS:%d", ft_push_swap(i[j]));
+        else
+            printf("PS:%d\n", ft_push_swap(i[j]));
+        j++;
+    }
     return 0;
 }
